fd7881ff14efcc72cc472028675456c85f680b55
Marcos Mont <78879585+404mqs@users.noreply.github.com>
2021-04-03 00:36:29 -0300

    Add files via upload

---
+++ RestrictedItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SuperRestrictor
+{
+    public class RestrictedItem
+    {
+        [XmlText]
+        public ushort Id { get; set; }
+        [XmlAttribute("BypassPermission")]
+        public string Bypass { get; set; }
+        [XmlAttribute("Message")]
+        public string Message { get; set; }
+
+
+
+        public RestrictedItem() { }
+    }
+}
