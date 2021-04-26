1aea4887f675e1e80049ed6bdfaa09418e4e7dd3
Marcos Mont <78879585+404mqs@users.noreply.github.com>
2021-04-26 09:00:19 -0300

    Add files via upload

---
+++ RestrictedCraft.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SuperRestrictor
+{
+    public class RestrictedCraft
+    {
+        [XmlText]
+        public ushort Id { get; set; }
+        [XmlAttribute("BypassPermission")]
+        public string Bypass { get; set; }
+        [XmlAttribute("Message")]
+        public string Message { get; set; }
+
+        public RestrictedCraft() { }
+    }
+}
